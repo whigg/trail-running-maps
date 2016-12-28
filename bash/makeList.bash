cd2b3e6327fa8005a2a4ce8cbbb1b74613e15fdb
Charles Troupin <ctroupin@socib.es>
2016-12-28 18:05:19 +0100

    added argin control

--- bash/makeList.bash
+++ bash/makeList.bash
@@ -1,8 +1,29 @@
 #!/bin/bash
-inputdir=$1
-outputfile=$2
 
-echo "var moves = [" > ${outputfile}
+if [ "$#" == "0" ]
+  then 
+  echo 'Usage: ... inputdir outputfile movename'
+  else if [ "$#" == "1" ]
+  then
+  inputdir=$1
+  outputfile='./'
+  movename='moves'
+  else if [ "$#" == "2" ] 
+  then
+  inputdir=$1
+  outputfile=$2
+  movename='moves'
+  else if [ "$#" == "3" ]
+  then
+  inputdir=$1
+  outputfile=$2
+  movename=$3
+  fi
+  fi
+  fi
+fi
+
+echo "var" ${movename} "= [" > ${outputfile}
 find ${inputdir} -maxdepth 1 -type f -name '*gpx' -printf '"%p",\n' >> ${outputfile}
 echo "]" >> ${outputfile}
 
