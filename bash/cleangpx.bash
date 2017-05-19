1953265f01c1c4665952f17d0abd91ed12550780
ctroupin <charles.troupin@gmail.com>
2017-05-19 10:42:58 +0200

    defined input and output files

--- bash/cleangpx.bash
+++ bash/cleangpx.bash
@@ -5,7 +5,15 @@
 # The GPX downloaded from Garmin or Movescount are often much bigger than necessary
 # because of information stored in <extension> tags. 
 # The script use 'sed' to remove what lies within these tags.
+
+
 TAG='extensions'
-cat config.xml-ori | sed -e '/<app-tag>/, /<\/app-tag>/ d ' > config.xml
+for files in "$@"
+do
+    outdir=$(dirname ${files})
+    outputfile=${outdir}'/'$(basename -s '.gpx' ${files})'_noext.gpx'
+    echo "cat ${files} | sed -e '/<${TAG}>/, /<\/${TAG}>/ d ' > ${outputfile}"
+done
+#cat config.xml-ori | sed -e '/<app-tag>/, /<\/app-tag>/ d ' > config.xml
 
 
