cd7006423d785d68f91ffdb62313afa712c86c44
ctroupin <charles.troupin@gmail.com>
2017-07-08 20:53:03 +0200

    script to reduce track density

--- bash/simplifytrack.bash
+++ bash/simplifytrack.bash
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-if [ "$#" -eq "1" ]; then
-  N=$1
-else
-  N=500
-fi
-
 GPSBABEL='/usr/bin/gpsbabel'
 
-for gpx in $(ls */*.gpx)
-do
-  ${GPSBABEL} -r -i gpx -f ${gpx} -x simplify,count=${N} -o gpx -F ${gpx}
+re='^[0-9]+$'
+
+# If last element is an integer,
+# then it's the number of points for the subsampling
+
+if ! [[ "${@: -1}" =~ "$re" ]] ; then
+  echo "that's an integer"
+  N=${@: -1}
+else
+  N=500  
+fi
+
+
+for gpx in $@ ; do
+  if [ -f ${gpx} ]; then 
+    echo "ok ok ok" 
+    echo ${GPSBABEL} -r -i gpx -f ${gpx} -x simplify,count=${N} -o gpx -F ${gpx}
+  fi
 done
