1d81333b9bac79fa02eecb5fa872ed2f2027bf82
ctroupin <charles.troupin@gmail.com>
2017-08-27 15:28:35 +0200

    simplify track script

--- bash/simplifytrack.bash
+++ bash/simplifytrack.bash
@@ -6,18 +6,20 @@ re='^[0-9]+$'
 
 # If last element is an integer,
 # then it's the number of points for the subsampling
+#
+#if ! [[ "${@: -1}" =~ "$re" ]] ; then
+#  echo "that's an integer"
+#  N=${@: -1}
+#else
+#  N=500  
+#fi
 
-if ! [[ "${@: -1}" =~ "$re" ]] ; then
-  echo "that's an integer"
-  N=${@: -1}
-else
-  N=500  
-fi
-
-
+N=500
 for gpx in $@ ; do
   if [ -f ${gpx} ]; then 
-    echo "ok ok ok" 
-    echo ${GPSBABEL} -r -i gpx -f ${gpx} -x simplify,count=${N} -o gpx -F ${gpx}
+    gpxout=${gpx%.*}"_500pts.gpx"
+    "${GPSBABEL}" -r -i gpx -f "${gpx}" -x simplify,count="${N}" -o gpx -F "${gpxout}"
+  else
+    echo "${gpx}" "doesn't exist"
   fi
 done
